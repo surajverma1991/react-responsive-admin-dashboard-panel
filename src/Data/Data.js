82e1d238f09700606d5633d940a75a2548bb2eda
Suraj Verma <vermasuraj833@gmail.com>
2024-05-06 20:55:43 +0530

    Init and Add Sidebar Menu

---
+++ src/Data/Data.js
@@ -0,0 +1,31 @@
+import {
+    UilEstate,
+    UilClipboardAlt,
+    UilUsersAlt,
+    UilPackage,
+    UilChart,
+} from '@iconscout/react-unicons'
+
+
+export const SidebarData = [
+    {
+        icon: UilEstate,
+        heading: "Dashboard"
+    },
+    {
+        icon: UilClipboardAlt,
+        heading: "Orders"
+    },
+    {
+        icon: UilUsersAlt,
+        heading: "Customer"
+    },
+    {
+        icon: UilPackage,
+        heading: "Products"
+    },
+    {
+        icon: UilChart,
+        heading: "Analytics"
+    }
+]
